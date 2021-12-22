fc37e17b5f4362fad793af3bb25dcac204dab110
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2021-12-22 23:43:40 +0530

    added jsx for home component

---
+++ Home/Home.jsx
@@ -0,0 +1,63 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { VideoCard } from '../VideoCard';
+import { useAppDataContext } from '../../Context';
+import PulseLoader from 'react-spinners/PulseLoader';
+
+export const Home = () => {
+  const { state } = useAppDataContext();
+  return (
+    <div className="home-container">
+      <div className="home-main">
+        <div className="discover-container">
+          <div className="discover-head">Discover</div>
+          <div className="discover-main">
+            <div className="discover-photo">
+              <p>
+                India has a rich,
+                <br />
+                Illustrous,Resplendent and Abundant
+                <br />
+                textile industry
+                <br />
+              </p>
+              <Link to="/explore?cat=Basics">
+                <button className="discover-button">Know How</button>
+              </Link>
+            </div>
+            <div className="discover-edit">
+              <p>
+                Use Eco-friendly fabrics
+                <br />
+                and yet have a dapper wardrobe,
+                <br />
+                do your planet a favour
+              </p>
+              <Link to="/explore?cat=Video%20Editing">
+                <button className="discover-button">Know More</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="popular-container">
+          <div className="popular-head">Popular</div>
+          <div
+            className={`${
+              state.videos.length !== 0 ? 'popular-main' : 'loader-container'
+            }`}
+          >
+            {state.videos.length !== 0 ? (
+              state.videos
+                .filter((video) => video.category === 'Popular')
+                .map((video) => {
+                  return <VideoCard video={video} key={video._id} />;
+                })
+            ) : (
+              <PulseLoader loading={true} size={15} color={'#6c5ecf'} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
