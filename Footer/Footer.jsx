4a5897b9033f6b66f6a73d8a7e556ad3e7fb4f9f
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2021-12-22 20:50:46 +0530

    made the footer component and added styles along with it

---
+++ Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import "./styles.css";
+import { Link } from "react-router-dom";
+import { FaYoutube } from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-main">
+        <div className="footer-left ">
+          <div className="footer-padding footer-para">Reach out to me here:</div>
+          <div className="socialNav">
+            <a
+              href="https://www.linkedin.com/in/srividyamv/"
+              aria-label="Linkedin-profile"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+            <a
+              href="https://twitter.com/MysoreSrividya"
+              aria-label="Twitter-handle"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://github.com/srivcodes" aria-label="Github-profile">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <Link to="/" className="footer-logo">
+            <FaYoutube className="head-logo" />
+            Watch
+          </Link>
+          <div className="connectMe">
+            <div className="footer-name-padding">BY</div>
+            <a
+              href="https://sounds-interesting.netlify.app/"
+              className=" footer-name-padding"
+            >
+              @SRIVI
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
