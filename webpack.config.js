fbe578db5a8ab8d33917e9e04532bd23fff43dad
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-13 19:04:37 +0530

    added new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'index.bundle.js',
+    // Need to add this otherwise routing won't work
+    publicPath: '/'
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
@@ -13,13 +16,13 @@ module.exports = {
       Utils: path.resolve(__dirname, 'src/Utils/'),
       Context: path.resolve(__dirname, 'src/Context/'),
       Components: path.resolve(__dirname, 'src/Components/'),
-      Reducer: path.resolve(__dirname, 'src/Reducer/'),
-    },
+      Reducer: path.resolve(__dirname, 'src/Reducer/')
+    }
   },
   devServer: {
     port: 3000,
     static: './public',
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   module: {
     rules: [
@@ -27,18 +30,23 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html')
+    })
+  ]
 };
